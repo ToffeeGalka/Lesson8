c5dd11bfad5a6bc763453653928be289115be167
ToffeeGalka <toffee@tut.by>
2024-02-28 15:42:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Кличка собаки/кота?");
+            string name = Console.ReadLine();
+
+            Animal dog = new Dog(name);
+            dog.Eat();
+            
+            Cat cat = new Cat(name);
+            var icat = (IAnimal)cat;
+            icat.Eat();
+        }
+    }
+}
